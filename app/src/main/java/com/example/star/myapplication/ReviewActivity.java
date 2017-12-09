8115b51b3a199d716d9ef1c9500d21b3c9ccfd6d
skdlzl326 <skdlzl326@naver.com>
2017-12-10 06:28:29 +0900

    사진촬영 불러오기 완료
    crop하는 과정에서의 화질 저하 문제

--- app/src/main/java/com/example/star/myapplication/ReviewActivity.java
+++ app/src/main/java/com/example/star/myapplication/ReviewActivity.java
@@ -1,23 +1,47 @@
 package com.example.star.myapplication;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 public class ReviewActivity extends AppCompatActivity {
+    private static final int PICK_FROM_CAMERA = 0;
+    private static final int PICK_FROM_ALBUM = 1;
+    private static final int CROP_FROM_IMAGE = 2;
+    private Uri mImageCaptureUri;
+    private ImageView iv_UserPhoto;
+    private int id_view;
+    private String absoultePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
+
+        iv_UserPhoto= (ImageView)this.findViewById(R.id.uploadimage);
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setBackgroundDrawable(new ColorDrawable(0xFF6CC6AD));
@@ -34,48 +58,161 @@ public class ReviewActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
         switch (item.getItemId()) {
             case android.R.id.home: {
                 onBackPressed();
                 return true;
             }
-            case R.id.post:{
-                Toast.makeText(this, "리뷰가 등록되었습니다.", Toast.LENGTH_LONG).show();
-            }
-
             case R.id.camera: {
                 AlertDialog.Builder alert_confirm = new AlertDialog.Builder(ReviewActivity.this);
-                alert_confirm.setMessage("업로드할 이미지 선택").setCancelable(false).setPositiveButton("사진촬영",
-                        new DialogInterface.OnClickListener() {
+                alert_confirm.setMessage("업로드할 이미지 선택").setCancelable(false)
+                        .setPositiveButton("사진촬영", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                // 'YES'
+                                doTakePhotoAction();
                             }
                         })
-                        .setNegativeButton("앨범선택",
-                        new DialogInterface.OnClickListener() {
+                        .setNegativeButton("앨범선택", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                // 'No'
+                                doTakeAlbumAction();
                                 return;
                             }
                         })
                         .setNeutralButton("취소", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                // 'No'
+                                dialog.dismiss();
                                 return;
                             }
                         });
                 AlertDialog alert = alert_confirm.create();
                 alert.show();
+                return true;
+            }
 
-
+            case R.id.post: {
+                Toast.makeText(this, "리뷰가 등록되었습니다.", Toast.LENGTH_LONG).show();
+                return true;
             }
 
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void doTakePhotoAction() // 카메라 촬영 후 이미지 가져오기
+    {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // 임시로 사용할 파일의 경로를 생성
+        String url = "tmp_" + String.valueOf(System.currentTimeMillis()) + ".jpg";
+        mImageCaptureUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(), url));
+
+        intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
+        startActivityForResult(intent, PICK_FROM_CAMERA);
+    }
+
+    public void doTakeAlbumAction() // 앨범에서 이미지 가져오기
+    {
+        // 앨범 호출
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
+        intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_FROM_ALBUM);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if(resultCode != RESULT_OK)
+            return;
+
+        switch(requestCode)
+        {
+            case PICK_FROM_ALBUM:
+            {
+                mImageCaptureUri = data.getData();
+                Log.d("SmartWheel",mImageCaptureUri.getPath().toString());
+
+            }
+
+            case PICK_FROM_CAMERA:
+            {
+                Intent intent = new Intent("com.android.camera.action.CROP");
+                intent.setDataAndType(mImageCaptureUri, "image/*");
+
+                // CROP할 이미지를 200*200 크기로 저장
+                intent.putExtra("outputX", 200); // CROP한 이미지의 x축 크기
+                intent.putExtra("outputY", 200); // CROP한 이미지의 y축 크기
+                intent.putExtra("aspectX", 1); // CROP 박스의 X축 비율
+                intent.putExtra("aspectY", 1); // CROP 박스의 Y축 비율
+                intent.putExtra("scale", true);
+                intent.putExtra("return-data", true);
+                startActivityForResult(intent, CROP_FROM_IMAGE); // CROP_FROM_CAMERA case문 이동
+                break;
+
+            }
+            case CROP_FROM_IMAGE:
+            {
+                if(resultCode != RESULT_OK) {
+                    return;
+                }
+
+                final Bundle extras = data.getExtras();
+
+                // CROP된 이미지를 저장하기 위한 FILE 경로
+                String filePath = Environment.getExternalStorageDirectory().getAbsolutePath()+
+                        "/Fooriend/"+System.currentTimeMillis()+".jpg";
+
+                if(extras != null)
+                {
+                    Bitmap photo = extras.getParcelable("data"); // CROP된 BITMAP
+                    iv_UserPhoto.setImageBitmap(photo); // 레이아웃의 이미지칸에 CROP된 BITMAP을 보여줌
+
+                    storeCropImage(photo, filePath); // CROP된 이미지를 외부저장소, 앨범에 저장한다.
+                    absoultePath = filePath;
+                    break;
+
+                }
+                // 임시 파일 삭제
+                File f = new File(mImageCaptureUri.getPath());
+                if(f.exists())
+                {
+                    f.delete();
+                }
+
+            }
+        }
+    }
+
+    private void storeCropImage(Bitmap bitmap, String filePath) {
+        String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath()+"/Fooriend";
+        File directory_Fooriend = new File(dirPath);
+        if(!directory_Fooriend.exists()) // Fooriend 디렉터리에 폴더가 없다면 (새로 이미지를 저장할 경우에 속한다.)
+            directory_Fooriend.mkdir();
+
+        File copyFile = new File(filePath);
+        BufferedOutputStream out = null;
+
+        try {
+            copyFile.createNewFile();
+            out = new BufferedOutputStream(new FileOutputStream(copyFile));
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+
+
+            sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,
+                    Uri.fromFile(copyFile)));
+
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
     @Override
     public void onBackPressed() {
             super.onBackPressed();
